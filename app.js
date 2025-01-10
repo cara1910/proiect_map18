c799b9c7a0924adaf31429c799bc7f63d4399a16
cara1910 <haiducarinam19@gmail.com>
2025-01-10 17:56:40 +0200

    update js

---
+++ app.js
@@ -0,0 +1,17 @@
+function getUserInput() {
+    //extrag imputurile
+    const randomInput = document.getElementById('randomInput').value;
+    const ascendingInput = document.getElementById('ascendingInput').value;
+    const descendingInput = document.getElementById('descendingInput').value;
+
+    //convertesc imputurile din stringuri in vectori de numere
+    const randomArr = randomInput.split(',').map(num => parseInt(num.trim())).filter(num => !isNaN(num));
+    const ascArr = ascendingInput.split(',').map(num => parseInt(num.trim())).filter(num => !isNaN(num));
+    const descArr = descendingInput.split(',').map(num => parseInt(num.trim())).filter(num => !isNaN(num));
+
+}
+
+document.getElementById('startButton').addEventListener('click', function() {
+    document.getElementById('rankingList').innerHTML = "";
+    getUserInput(); //sterg vechile rezultate
+});
